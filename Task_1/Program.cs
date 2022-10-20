fd35e7cf8b07d655de942121fb6b647bd061a4ca
Mikhail173 <2016.Nevermind.2020@gmail.com>
2022-10-20 22:30:11 +0300

    Palindrome

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input your 5-digit number");
+string n = Console.ReadLine();
+int length = n.Length;
+if (length != 5)
+{
+    Console.WriteLine("Wrong number!");
+}
+
+if (n[0] == n[4] && n[1] == n[3])
+{
+    Console.WriteLine("Your number is a palindrome!");
+}
+else
+{
+    Console.WriteLine("Your number is not a palindrome!");
+}
